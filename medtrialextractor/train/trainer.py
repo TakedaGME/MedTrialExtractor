e29038e2695c621b1d407e84095e9098a5e49202
Juan M Ortiz <juanmoo@me.com>
2021-05-18 14:33:48 -0400

    Update to trainer file to use transformers v4.6.0

--- medtrialextractor/train/trainer.py
+++ medtrialextractor/train/trainer.py
@@ -18,24 +18,36 @@ from transformers import PreTrainedModel
 from transformers import TrainingArguments
 from transformers.data.data_collator import DataCollator
 from transformers import AdamW, get_linear_schedule_with_warmup
+from transformers.optimization import Adafactor, get_scheduler
+from transformers.tokenization_utils_base import PreTrainedTokenizerBase
 
+from transformers.trainer_utils import (
+    EvalPrediction,
+)
+
+from transformers.trainer_callback import (
+    TrainerCallback,
+)
 
 logger = logging.getLogger(__name__)
-
 
 class IETrainer(Trainer):
     """
     IETrainer is inheritated from from transformers.Trainer, optimized for IE tasks.
     """
+
     def __init__(
         self,
-        model: PreTrainedModel,
-        args: TrainingArguments,
+        model: Union[PreTrainedModel, torch.nn.Module] = None,
+        args: TrainingArguments = None,
         data_collator: Optional[DataCollator] = None,
         train_dataset: Optional[Dataset] = None,
         eval_dataset: Optional[Dataset] = None,
-        compute_metrics=None,
-        optimizers: Tuple[torch.optim.Optimizer, torch.optim.lr_scheduler.LambdaLR] = None,
+        tokenizer: Optional[PreTrainedTokenizerBase] = None,
+        model_init: Callable[[], PreTrainedModel] = None,
+        compute_metrics: Optional[Callable[[EvalPrediction], Dict]] = None,
+        callbacks: Optional[List[TrainerCallback]] = None,
+        optimizers: Tuple[torch.optim.Optimizer, torch.optim.lr_scheduler.LambdaLR] = (None, None),
         use_crf: Optional[bool]=False
     ):
         super(IETrainer, self).__init__(
@@ -45,9 +57,112 @@ class IETrainer(Trainer):
             train_dataset=train_dataset,
             eval_dataset=eval_dataset,
             compute_metrics=compute_metrics,
-            optimizers=optimizers
+            optimizers=optimizers,
+            tokenizer=tokenizer,
+            model_init=model_init,
+            callbacks=callbacks
         )
         self.use_crf = use_crf
+
+    def create_optimizer_and_scheduler(self, num_training_steps: int):
+        """
+        Setup the optimizer and the learning rate scheduler.
+
+        We provide a reasonable default that works well. If you want to use something else, you can pass a tuple in the
+        Trainer's init through :obj:`optimizers`, or subclass and override this method (or :obj:`create_optimizer`
+        and/or :obj:`create_scheduler`) in a subclass.
+        """
+        self.create_optimizer()
+        self.create_scheduler(num_training_steps)
+
+    def create_scheduler(self, num_training_steps: int):
+        """
+        Setup the scheduler. The optimizer of the trainer must have been set up before this method is called.
+
+        Args:
+            num_training_steps (int): The number of training steps to do.
+        """
+        if self.lr_scheduler is None:
+            warmup_steps = (
+                self.args.warmup_steps
+                if self.args.warmup_steps > 0
+                else math.ceil(num_training_steps * self.args.warmup_ratio)
+            )
+
+            self.lr_scheduler = get_scheduler(
+                self.args.lr_scheduler_type,
+                self.optimizer,
+                num_warmup_steps=warmup_steps,
+                num_training_steps=num_training_steps,
+            )
+
+    def create_optimizer(self):
+        """
+        Setup the optimizer.
+
+        We provide a reasonable default that works well. If you want to use something else, you can pass a tuple in the
+        Trainer's init through :obj:`optimizers`, or subclass and override this method in a subclass.
+        """
+        if self.optimizer is None:
+
+            no_decay = ["bias", "LayerNorm.weight"]
+            if self.use_crf:
+
+                crf = "crf"
+                crf_lr = self.args.crf_learning_rate
+                logger.info(f"Learning rate for CRF: {crf_lr}")
+                optimizer_grouped_parameters = [
+                    {
+                        "params": [
+                            p for n, p in self.model.named_parameters()
+                            if (not any(nd in n for nd in no_decay)) and (crf not in n)
+                        ],
+                        "weight_decay": self.args.weight_decay
+                    },
+                    {
+                        "params": [p for p in self.model.crf.parameters()],
+                        "weight_decay": self.args.weight_decay,
+                        "lr": crf_lr
+                    },
+                    {
+                        "params": [
+                            p for n, p in self.model.named_parameters()
+                            if any(nd in n for nd in no_decay) and (not crf not in n)
+                        ],
+                        "weight_decay": 0.0,
+                    },
+                ]
+            else:
+                optimizer_grouped_parameters = [
+                    {
+                        "params": [
+                            p for n, p in self.model.named_parameters()
+                            if not any(nd in n for nd in no_decay)
+                        ],
+                        "weight_decay": self.args.weight_decay,
+                    },
+                    {
+                        "params": [
+                            p for n, p in self.model.named_parameters()
+                            if any(nd in n for nd in no_decay)
+                        ],
+                        "weight_decay": 0.0,
+                    },
+                ]
+
+
+            optimizer_cls = Adafactor if self.args.adafactor else AdamW
+            if self.args.adafactor:
+                optimizer_cls = Adafactor
+                optimizer_kwargs = {"scale_parameter": False, "relative_step": False}
+            else:
+                optimizer_cls = AdamW
+                optimizer_kwargs = {
+                    "betas": (self.args.adam_beta1, self.args.adam_beta2),
+                    "eps": self.args.adam_epsilon,
+                }
+            optimizer_kwargs["lr"] = self.args.learning_rate
+            self.optimizer = optimizer_cls(optimizer_grouped_parameters, **optimizer_kwargs)
 
     def get_optimizers(
         self,
@@ -116,11 +231,17 @@ class IETrainer(Trainer):
 
         return optimizer, scheduler
 
-    def evaluate(self, eval_dataset: Optional[Dataset] = None) -> Dict:
+    def evaluate(
+        self,
+        eval_dataset: Optional[Dataset] = None,
+        ignore_keys: Optional[List[str]] = None,
+        metric_key_prefix: str = "eval",
+    ) -> Dict[str, float]:
+
         eval_dataloader = self.get_eval_dataloader(eval_dataset)
         output = self._prediction_loop(eval_dataloader, description="Evaluation")
 
-        self._log(output['metrics'])
+        self.log(output['metrics'])
 
         return output
 
@@ -195,20 +316,34 @@ class IETrainer(Trainer):
 
         return {'predictions': preds_ids, 'label_ids': label_ids, 'metrics': metrics}
 
-    def _log(self, logs: Dict[str, float], iterator: Optional[tqdm] = None) -> None:
-        if self.epoch is not None:
-            logs["epoch"] = self.epoch
-        if self.global_step is None:
+
+    def log(self, logs: Dict[str, float], iterator: Optional[tqdm] = None) -> None:
+        """
+        Log :obj:`logs` on the various objects watching training.
+
+        Subclass and override this method to inject custom behavior.
+
+        Args:
+            logs (:obj:`Dict[str, float]`):
+                The values to log.
+        """
+        if self.state.epoch is not None:
+            logs["epoch"] = round(self.state.epoch, 2)
+        
+        if self.state.global_step is None:
             # when logging evaluation metrics without training
-            self.global_step = 0
-        # if is_wandb_available():
-        #     if self.is_world_master():
-        #         wandb.log(logs, step=self.global_step)
-        output = {**logs, **{"step": self.global_step}}
+            self.state.global_step = 0
+
+        output = {**logs, **{"step": self.state.global_step}}
+
         if iterator is not None:
             iterator.write(output)
+        
         else:
             logger.info(
                 {k:round(v, 4) if isinstance(v, float) else v for k, v in output.items()}
             )
+
+        self.state.log_history.append(output)
+        self.control = self.callback_handler.on_log(self.args, self.state, self.control, logs)
 
